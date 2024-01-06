5a7434681e88bfb0b94fcf9646ade237c3a13db6
DESKTOP-G0TT3JA\Usuário <joao.jbosco@gmail.com>
2024-01-06 15:47:31 -0300

    Add files.

--- ConsoleOperadorAritmeticos/Program.cs
+++ ConsoleOperadorAritmeticos/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
  Console.WriteLine("Hello, World!");
 
 int x = 3 + 4 * 2; // 11
@@ -16,7 +17,6 @@ Console.WriteLine(w);
 double k = 10.0 / 8;// essa divisão retornará um double pois existe pelo menos um double na divisão = 1,25
 Console.WriteLine(k);
 
-
 //Criando uma equação do 2º Grau, formula de báscara
 
 double a = 1.0, b = -3.0, c = -4.0;
@@ -24,3 +24,4 @@ double delta = b * b - 4.0 * a * c; // or double delta = Math.Pow(b,2) - 4.0 * a
 double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
 Console.WriteLine(delta);
 Console.WriteLine(x1);
+
